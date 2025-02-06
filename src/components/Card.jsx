d78eafc14700dff12c6f2defacb9e6705d8bbf8f
hermtechs <hermtechs@gmail.com>
2025-02-06 10:29:44 +0300

    created a reusable jobListing component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import HomeCards from "./HomeCards";
-const Card = ({ children, bg = "bg-gray-100" }) => {
+const Card = ({ children, bg = "bg-red-100 p-6 rounded-lg shadow-md" }) => {
   return <div className={`${bg} rounded-lg shadow-md`}>{children}</div>;
 };
 export default Card;
--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -4,7 +4,7 @@ const HomeCards = ({ children, bg }) => {
   return (
     <div className="container-xl lg:container m-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-        <Card>
+        <Card bg={"bg-gray-100 p-6 rounded-lg shadow-md"}>
           <h2 className="text-2xl font-bold">For Developers</h2>
           <p className="mt-2 mb-4">
             Browse our React jobs and start your career today
@@ -16,7 +16,7 @@ const HomeCards = ({ children, bg }) => {
             Browse Jobs
           </a>
         </Card>
-        <Card bg={"bg-red-100 p-6 rounded-lg shadow-md"}>
+        <Card>
           <h2 className="text-2xl font-bold">For Employers</h2>
           <p className="mt-2 mb-4">
             List your job to find the perfect developer for the role
